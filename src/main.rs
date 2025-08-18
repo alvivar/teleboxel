7d86f87c7492b3b88d90bd85645ba3aafe1a1e26
Andres Villalobos <andresalvivar@gmail.com>
2025-08-18 01:16:05 -0600

    Trying to make Tokio, Axum and Fastwebsockets to work together.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use axum::{Router, response::IntoResponse, routing::get};
+use fastwebsockets::{OpCode, WebSocketError, upgrade};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(ws_handler));
+}
+
+async fn ws_handler(ws: upgrade::IncomingUpgrade) -> impl IntoResponse {
+    let (response, fut) = ws.upgrade().unwrap();
+    tokio::task::spawn(async move {
+        if let Err(e) = handle_client(fut).await {
+            eprintln!("Error handling client: {}", e);
+        }
+    });
+    response
+}
+
+async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
+    let mut ws = fastwebsockets::FragmentCollector::new(fut.await?);
+    loop {
+        let frame = ws.read_frame().await?;
+        match frame.opcode {
+            OpCode::Close => break,
+            OpCode::Text | OpCode::Binary => {
+                ws.write_frame(frame).await?;
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
